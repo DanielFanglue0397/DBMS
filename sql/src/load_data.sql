d92f6f060069a90a9580dceeb783dc654d38c76f
FANGLUE LIU <fliu084@wch133-23.cs.ucr.edu>
2023-03-16 15:14:46 -0700

    gitignore

--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -1,36 +1,36 @@
 COPY Users
-FROM '/extra/kchen329/project/data/users.csv' 
+FROM '/extra/fliu084/project/data/users.csv' 
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE users_userID_seq RESTART 101; 
 
 COPY Hotel
-FROM '/extra/kchen329/project/data/hotels.csv'
+FROM '/extra/fliu084/project/data/hotels.csv'
 WITH DELIMITER ',' CSV HEADER;   
 
 COPY Rooms
-FROM '/extra/kchen329/project/data/rooms.csv'
+FROM '/extra/fliu084/project/data/rooms.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY MaintenanceCompany
-FROM '/extra/kchen329/project/data/company.csv'
+FROM '/extra/fliu084/project/data/company.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY RoomBookings
-FROM '/extra/kchen329/project/data/bookings.csv'
+FROM '/extra/fliu084/project/data/bookings.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE RoomBookings_bookingID_seq RESTART 501; 
 
 COPY RoomRepairs
-FROM '/extra/kchen329/project/data/roomRepairs.csv'
+FROM '/extra/fliu084/project/data/roomRepairs.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE roomRepairs_repairID_seq RESTART 11;
 
 COPY RoomRepairRequests
-FROM '/extra/kchen329/project/data/roomRepairRequests.csv'
+FROM '/extra/fliu084/project/data/roomRepairRequests.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE roomRepairRequests_requestNumber_seq RESTART 11;
 
 COPY RoomUpdatesLog
-FROM '/extra/kchen329/project/data/roomUpdatesLog.csv'
+FROM '/extra/fliu084/project/data/roomUpdatesLog.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE roomUpdatesLog_updateNumber_seq RESTART 51;
