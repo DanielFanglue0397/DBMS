c1fd2fd2c30e7410a351c345ab77ae32f555ae85
Kenny Chen <kchen329@wch133-12.cs.ucr.edu>
2023-03-18 14:12:26 -0700

    no need tirgger for now ,triggers are comment out

--- sql/src/triggers.sql
+++ sql/src/triggers.sql
@@ -1,18 +1,18 @@
-DROP SEQUENCE IF EXISTS bookingID_seq;
-CREATE OR REPLACE LANGUAGE plpgsql;
-CREATE OR REPLACE FUNCTION return_bookingID()
-RETURN "trigger" AS
-$BODY$
-BEGIN
-    NEW.bookingID = nextval('bookingID_seq');
-    RETURN NEW;
-END;
-$BODY$
-LANGUAGE plpgsql VOLATILE;
+-- DROP SEQUENCE IF EXISTS bookingID_seq;
+-- CREATE OR REPLACE LANGUAGE plpgsql;
+-- CREATE OR REPLACE FUNCTION return_bookingID()
+-- RETURN "trigger" AS
+-- $BODY$
+-- BEGIN
+--     NEW.bookingID = nextval('bookingID_seq');
+--     RETURN NEW;
+-- END;
+-- $BODY$
+-- LANGUAGE plpgsql VOLATILE;
 
-DROP TRIGGER IF EXISTS bookingID ON RoomBookings;
-CREATE TRIGGER bookingID
-AFTER INSERT
-ON RoomBookings
-FOR EACH ROW
-EXECUTE PROCEDURE return_bookingID();
+-- DROP TRIGGER IF EXISTS bookingID ON RoomBookings;
+-- CREATE TRIGGER bookingID
+-- AFTER INSERT
+-- ON RoomBookings
+-- FOR EACH ROW
+-- EXECUTE PROCEDURE return_bookingID();
