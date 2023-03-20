ba23713b828169f22dd45e69c2f61a3f141bfb02
Kenny Chen <kchen329@wch133-12.cs.ucr.edu>
2023-03-20 13:04:51 -0700

    ascii art and wrong password or userid warning

--- java/src/Hotel.java
+++ java/src/Hotel.java
@@ -32,6 +32,25 @@ import java.lang.Math;
  */
 public class Hotel {
 
+   public static final String ANSI_RESET = "\u001B[0m";
+   public static final String ANSI_BLACK = "\u001B[30m";
+   public static final String ANSI_RED = "\u001B[31m";
+   public static final String ANSI_GREEN = "\u001B[32m";
+   public static final String ANSI_YELLOW = "\u001B[33m";
+   public static final String ANSI_BLUE = "\u001B[34m";
+   public static final String ANSI_PURPLE = "\u001B[35m";
+   public static final String ANSI_CYAN = "\u001B[36m";
+   public static final String ANSI_WHITE = "\u001B[37m";
+
+   public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+   public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+   public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+   public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+   public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+   public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+   public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+   public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
    // reference to physical database connection.
    private Connection _connection = null;
 
@@ -55,7 +74,7 @@ public class Hotel {
       try{
          // constructs the connection URL
          String url = "jdbc:postgresql://localhost:" + dbport + "/" + dbname;
-         System.out.println ("Connection URL: " + url + "\n");
+         System.out.println ("\nConnection URL: " + url);
 
          // obtain a physical connection
          this._connection = DriverManager.getConnection(url, user, passwd);
@@ -249,7 +268,6 @@ public class Hotel {
          return;
       }//end if
 
-      Greeting();
       Hotel esql = null;
       try{
          // use postgres JDBC driver.
@@ -260,9 +278,13 @@ public class Hotel {
          String dbport = args[1];
          String user = args[2];
          esql = new Hotel (dbname, dbport, user, "");
-
+         boolean attempted = false;
          boolean keepon = true;
+         Greeting();
          while(keepon) {
+            if (attempted) {
+               System.out.println(ANSI_RED +"\n⚠  We can't find that userID and password. Please try again." + ANSI_RESET);
+            }
             // These are sample SQL statements
             System.out.println("\nMAIN MENU");
             System.out.println("---------");
@@ -272,11 +294,12 @@ public class Hotel {
             String authorisedUser = null;
             switch (readChoice()){
                case 1: CreateUser(esql); break;
-               case 2: authorisedUser = LogIn(esql); break;
+               case 2: authorisedUser = LogIn(esql); attempted = true; break;
                case 9: keepon = false; break;
                default : System.out.println("\nUnrecognized choice!"); break;
             }//end switch
             if (authorisedUser != null) {
+               attempted = false;
                String query = "SELECT u.userType FROM Users u WHERE u.userID = " + authorisedUser;
                String userType = esql.executeQueryAndReturnResult(query).get(0).get(0);
                if (userType.toLowerCase().contains("customer")) {
@@ -357,10 +380,17 @@ public class Hotel {
    }//end main
 
    public static void Greeting(){
-      System.out.println(
-         "\n\n*******************************************************\n" +
-         "              User Interface      	               \n" +
-         "*******************************************************\n");
+      System.out.println();      
+      System.out.println();      
+      System.out.println();      
+      System.out.println("═══════════════════════ Welcome To ═══════════════════════\n");
+      System.out.println("        ██╗  ██╗ ██████╗ ████████╗███████╗██╗             ");
+      System.out.println("        ██║  ██║██╔═══██╗╚══██╔══╝██╔════╝██║             ");      
+      System.out.println("        ███████║██║   ██║   ██║   █████╗  ██║             ");      
+      System.out.println("        ██╔══██║██║   ██║   ██║   ██╔══╝  ██║             ");      
+      System.out.println("        ██║  ██║╚██████╔╝   ██║   ███████╗███████╗        ");      
+      System.out.println("        ╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝        ");      
+      System.out.println("\n═══════════════ Database management System ═══════════════");                                          
    }//end Greeting
 
    /*
